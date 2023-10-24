e331ff2e1e5917a61f2f35740fe7eb512a9bb383
musense <musense.ad@gmail.com>
2023-10-24 17:39:28 +0800

    add prisma and configuring

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction } from '@remix-run/node'
+import styles from './styles/app.css'
 import {
   Links,
   LiveReload,
@@ -7,18 +7,21 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react'
 
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }]
+}
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1'
+        />
         <Meta />
         <Links />
       </head>
@@ -29,5 +32,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
